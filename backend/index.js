325115b034b1085445b7240770d88efcc087d3f2
ethanpreyna <ethanpreyna@gmail.com>
2021-10-22 15:21:50 -0500

    Updated index.js in backend

--- backend/index.js
+++ backend/index.js
@@ -20,17 +20,49 @@ async function connect() {
         //await sequelize.authenticate();
         await client.connect();
         console.log('Connection has been established successfully.');
-        q = await client.query("select * from accounts;");
-        console.log(q.rows);
 
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
 }
 
+/*async function getCurrDate(){ //Gets current date to check if auth keys are at current date or past
+    //SQL Timestamp Format: YYYY-MM-DD HH24:MI:SSXFF
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var todayFin = yyyy +"-" + mm + "-" + dd + "T" + time;
+    
+    return today;
+}*/
+
+async function checkValidDates(){ //Goes through all rows in github_info and checks to see if each PAT expir date has passed. Removes from table if date passed.
+    q = await client.query("SELECT * FROM github_info");
+    today = new Date();
+    expiredUsers = [];
+    for(var i = 0; i < q.rows.length; i++){
+        //currDateValue = String(q.rows[i].pat_token_expiration_date);
+        if(q.rows[i].pat_token_expiration_date < today){
+            console.log("Expired PAT for user: " + q.rows[i].github_username + ", Expired on: " + String(q.rows[i].pat_token_expiration_date));
+            expiredUsers.push(q.rows[i].github_username);
+        }
+    }
+    for(var i = 0; i < expiredUsers.length; i++){
+        try{
+            q = await client.query(`DELETE FROM github_info WHERE github_username = '${expiredUsers[i]}'`);
+        } catch(error){
+            console.error('Could not deletee from table: ', error);
+        }
+    }
+}
+
 
 async function main(){
     await connect();
+    //today = await getCurrDate();
+    await checkValidDates();
     await client.end();
     process.exit;
 }
