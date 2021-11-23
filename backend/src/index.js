ae03e4d399ba575749f947855bb18ec437a91cd7
dogServer <dogunbound5@gmail.com>
2021-11-23 03:44:35 +0000

    fixed cors policy problem

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,7 @@ import timeline from './timeline.js';
 
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
 
