83d6ff07bb85d74e76ca5655ca2c9949acacb00e
dogunbound <dogunbound5@gmail.com>
2021-11-30 16:16:48 -0600

    fix for database.js to allow docker run

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,8 +1,8 @@
 import pg from 'pg';
-const host = process.env.NODE_ENV == "test" ? process.env.DB_HOST : localhost
+
 const pool = new pg.Pool({
 	user: process.env.DB_USER,
-	host: host,
+	host: process.env.DB_HOST,
 	database: process.env.DB_NAME,
 	password: process.env.DB_PASS,
 	port: process.env.DB_PORT
